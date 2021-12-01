88071b997f6a5cf73ef99e7e52161a2e233dbe97
Rawad01 <92724617+Rawad01@users.noreply.github.com>
2021-12-01 19:20:11 +0200

    apis

---
+++ apis/connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "applicationdb";
+
+$cnnx = new mysqli($server, $username, $password, $dbname);
+
+if($cnnx->connect_error){
+	die("connection failed :)");
+}
+
+?>
